58ee9bb2d81f43d27541c21d25b416edb6c2a9a1
m-banik <mrn.banik@gmail.com>
2022-08-10 01:52:30 +0200

    Implement the first Jest based tests

---
+++ src/utilities/getMinutesAndSecondsFromDurationInSeconds.test.ts
@@ -0,0 +1,39 @@
+import { getMinutesAndSecondsFromDurationInSeconds } from '.';
+
+describe('getMinutesAndSecondsFromDurationInSeconds', () => {
+  describe('for duration shorter than 1 minute', () => {
+    const instance = getMinutesAndSecondsFromDurationInSeconds(30);
+
+    it('returns 0 minutes when duration lasts 30 seconds', () => {
+      expect(instance[0]).toBe(0);
+    });
+
+    it('returns 30 seconds when duration lasts 30 seconds', () => {
+      expect(instance[1]).toBe(30);
+    });
+  });
+
+  describe('for duration equal to 1 minute', () => {
+    const instance = getMinutesAndSecondsFromDurationInSeconds(60);
+
+    it('returns 1 minutes when duration lasts 60 seconds', () => {
+      expect(instance[0]).toBe(1);
+    });
+
+    it('returns 0 seconds when duration lasts 60 seconds', () => {
+      expect(instance[1]).toBe(0);
+    });
+  });
+
+  describe('for duration longer than 1 minute', () => {
+    const instance = getMinutesAndSecondsFromDurationInSeconds(30);
+
+    it('returns 2 minutes when duration lasts 140 seconds', () => {
+      expect(instance[0]).toBe(0);
+    });
+
+    it('returns 20 seconds when duration lasts 140 seconds', () => {
+      expect(instance[1]).toBe(30);
+    });
+  });
+});
