dddee67c31e186c910d95fa3507e7aa6b51c8494
m-banik <mrn.banik@gmail.com>
2022-08-27 23:56:20 +0200

    Add timebox related type guards and some new types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,7 +1,9 @@
 import { ChangeEventHandler, MouseEventHandler } from 'react';
-import { nanoid } from 'nanoid';
+// ToDo: Changed due to the outer API
+// import { nanoid } from 'nanoid';
 
-export type IdType = ReturnType<typeof nanoid>;
+export type IdType = number;
+// export type IdType = ReturnType<typeof nanoid>;
 
 export type TimeboxType = {
   id: IdType;
@@ -13,8 +15,21 @@ export type TimeboxHandlerType = (timebox: TimeboxType) => void;
 
 export type TimeboxDataType = Omit<TimeboxType, 'id'>;
 
+export type PartialTimeboxType = Partial<TimeboxType> & { id: IdType };
+
 export type TimeboxDataHandlerType = (timebox: TimeboxDataType) => void;
 
 export type InputChangeEventHandlerType = ChangeEventHandler<HTMLInputElement>;
 
 export type ButtonEventHandlerType = MouseEventHandler<HTMLButtonElement>;
+
+export type TimeboxesApiType = {
+  getTimebox: (timeboxId: IdType) => Promise<TimeboxType>;
+  getTimeboxes: () => Promise<TimeboxType[]>;
+  addTimebox: (addedTimeboxData: TimeboxDataType) => Promise<TimeboxType>;
+  editTimebox: (editedTimebox: TimeboxType) => Promise<TimeboxType>;
+  partiallyUpdateTimebox: (
+    partiallyUpdatedTimebox: PartialTimeboxType
+  ) => Promise<TimeboxType>;
+  removeTimebox: (removedTimeboxId: IdType) => Promise<unknown>;
+};
--- src/utilities/typeGuards.ts
+++ src/utilities/typeGuards.ts
@@ -1,4 +1,5 @@
 import { CSSProperties } from 'react';
+import { TimeboxType, TimeboxDataType } from '../common';
 
 export function assertObjectIsOfCssVariablesPropertiesType(
   input: unknown,
@@ -16,5 +17,98 @@ export function assertObjectIsOfCssVariablesPropertiesType(
     if (!areVariablesOfRightFormat) {
       throw new Error(errorMessage);
     }
+  }
+}
+
+export const checkIfIsOfTimeboxType = (
+  input: unknown
+): input is TimeboxType => {
+  const instance = input as TimeboxType;
+
+  return (
+    typeof instance === 'object' &&
+    !(instance instanceof Array) &&
+    typeof instance.id === 'number' &&
+    typeof instance.title === 'string' &&
+    typeof instance.totalTimeInMinutes === 'number'
+  );
+};
+
+export const checkIfAreOfTimeboxType = (
+  input: unknown
+): input is TimeboxType[] => {
+  const instance = input as TimeboxType[];
+
+  return (
+    instance instanceof Array &&
+    instance.every((element) => checkIfIsOfTimeboxType(element))
+  );
+};
+
+export function asssertIsOfTimeboxType(
+  input: unknown,
+  message = 'Incorrect data type!'
+): asserts input is TimeboxType {
+  const instance = input as TimeboxType;
+
+  if (!checkIfIsOfTimeboxType(instance)) {
+    throw new Error(message);
+  }
+}
+
+export function asssertAreOfTimeboxType(
+  input: unknown,
+  message = 'Incorrect data type!'
+): asserts input is TimeboxType[] {
+  const instance = input as TimeboxType[];
+
+  if (!checkIfAreOfTimeboxType(instance)) {
+    throw new Error(message);
+  }
+}
+
+export const checkIfIsOfTimeboxDataType = (
+  input: unknown
+): input is TimeboxDataType => {
+  const instance = input as TimeboxDataType;
+
+  return (
+    typeof instance === 'object' &&
+    !(instance instanceof Array) &&
+    typeof instance.title === 'string' &&
+    typeof instance.totalTimeInMinutes === 'number'
+  );
+};
+
+export const checkIfAreOfTimeboxDataType = (
+  input: unknown
+): input is TimeboxDataType[] => {
+  const instance = input as TimeboxDataType[];
+
+  return (
+    instance instanceof Array &&
+    instance.every((element) => checkIfIsOfTimeboxDataType(element))
+  );
+};
+
+export function asssertIsOfTimeboxDataType(
+  input: unknown,
+  message = 'Incorrect data type!'
+): asserts input is TimeboxDataType {
+  const instance = input as TimeboxDataType;
+
+  if (!checkIfIsOfTimeboxDataType(instance)) {
+    throw new Error(message);
+  }
+}
+
+export function asssertAreOfTimeboxDataType(
+  input: unknown,
+  message = 'Incorrect data type!'
+): asserts input is TimeboxDataType[] {
+  const instance = input as TimeboxDataType[];
+
+  if (!checkIfAreOfTimeboxDataType(instance)) {
+    throw new Error(message);
   }
 }
